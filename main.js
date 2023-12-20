22f87eb7b8fea9ddb61060777a1530f42b90aaba
Nikolay Panovski <nikipanovski@gmail.com>
2023-12-20 17:33:04 +0200

    [Non-functional] Run MQTT client + accept the messages in renderer
    WARNING: This commit will not work out of the box because main.js does not respect process.env for some reason. The static username and password will be stashed in and out when necessary, outside of commits.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("node:path");
 
+const mqtt = require("mqtt");
+const totoRepoId = "e0c1defb-c75d-4c3b-8ef2-69d7ec8e224c";      // City and City
+const totoHubId = "2e44ba80-6d28-4dac-a365-75cdb0fa7ebc";       // NP-ScrMult/100-SignalHub
+
+let win;
+
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -12,9 +18,43 @@ const createWindow = () => {
   
     win.loadFile('index.html');
 }
+
+function doMqttConnect() {
+    const client = mqtt.connect("tls://mqtt.toto.io:8883", {
+        //rejectUnauthorized: false,
+        username: process.env.TOTO_OBJECTS_USERNAME,     // for some reason does not get transmitted from .env
+        password: process.env.TOTO_OBJECTS_PASSWORD,
+    });
+    console.log(client);
+
+
+    client.on('connect', () => {
+        console.log('Connected to MQTT broker');
+      
+        client.subscribe('toto/' + totoRepoId + '/' + totoHubId + '/in' );
+
+        win.webContents.send("mqtt", "Hello from main!");
+    });
+      
+
+    client.on('message', (topic, message) => {
+        console.log(`-- MESSAGE TOPIC: -- ${topic}`);
+      
+        // send message as received, renderer uses ids to filter it and deal with it
+        win.webContents.send("mqtt", message.toString());
+    });
+      
+    // Handle disconnects
+    client.on('close', () => {
+        console.log('Disconnected from MQTT broker');
+        client.end();
+        process.exit();
+    });
+}
   
+
 app.whenReady().then(() => {
-    ipcMain.handle("ping", () => "pong");
     createWindow();
+    doMqttConnect();
 });
 
--- renderer.js
+++ renderer.js
@@ -1,10 +1,29 @@
+
+// INSTEAD:
+// get browser tab image
+// render the actual image or full black depending on MQTT messaging
 const information = document.getElementById('info');
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+information.innerText = "Hold on,";
+/*`This app is using Chrome (v${tutorial.chrome()}), Node.js (v${tutorial.node()}), and Electron (v${tutorial.electron()})`*/
 
 
+/**
 const func = async () => {
-    const response = await window.versions.ping();
+    const response = await window.tutorial.ping();
     console.log(response); // prints out 'pong'
 }
-  
-func();
+/**/
+
+window.mqttAPI.onMqttMessage((message) => {
+    console.log("Message at renderer: ");
+    console.log(message);
+    
+    if (message === window.mqttAPI.totoObjectId) {
+        console.log("Good! Now add the image");
+        // set relevant document image element to proper source / full opacity
+    }
+    else {
+        console.log("Fine! Page should go black");
+        // set relevant document image element to black
+    }
+});
