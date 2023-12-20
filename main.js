73d3c7dc7fd8269b37568479ef826365973bed2e
Nikolay Panovski <nikipanovski@gmail.com>
2023-12-20 14:57:04 +0200

    Add sample main page from tutorial

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello from Electron');
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    });
+  
+    win.loadFile('index.html');
+}
+  
+app.whenReady().then(() => {
+    createWindow();
+});
