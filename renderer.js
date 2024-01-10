ca235ff4800594cd474c6c7eda6361968941138b
NikolayP <nikipanovski@gmail.com>
2024-01-10 11:08:33 +0100

    Add dedicated images for Europe Square in a separate folder

--- renderer.js
+++ renderer.js
@@ -3,7 +3,7 @@ const body = document.getElementById("body");
 
 window.mqttAPI.onMqttMessage((message) => {
     if (message === window.mqttAPI.totoObjectId) {
-        body.style.backgroundImage = "url(./image.png)";
+        body.style.backgroundImage = "url(./imgs/EuropeSquare_BOTTOM_LEFT.png)";
     }
     else {
         body.style.backgroundImage = "none";
